1754b8f9c574d2e41c7b74df2d4e2796ee8144ef
Joe Kent <897368+itsjoekent@users.noreply.github.com>
2023-04-14 14:01:01 -0400

    fixing public folder

--- .eleventy.js
+++ .eleventy.js
@@ -2,8 +2,8 @@ const EleventyVitePlugin = require('@11ty/eleventy-plugin-vite');
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(EleventyVitePlugin);
-  eleventyConfig.addPassthroughCopy({ public: '/' });
 
+  eleventyConfig.addPassthroughCopy('public');
 	eleventyConfig.addPassthroughCopy('src/css');
 	eleventyConfig.addPassthroughCopy('src/js');
 
